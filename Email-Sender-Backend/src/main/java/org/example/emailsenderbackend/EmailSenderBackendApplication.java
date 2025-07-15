15940e60c20861fc73d0a21a44bb3064f60530b5
max125-c <pravintambe5671@gmail.com>
2025-07-16 03:32:26 +0530

    Initial commit

---
+++ Email-Sender-Backend/src/main/java/org/example/emailsenderbackend/EmailSenderBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.emailsenderbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailSenderBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailSenderBackendApplication.class, args);
+    }
+
+}
